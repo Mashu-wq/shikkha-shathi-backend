f739b849f51f98e32d68c8d2596db43e2adde5e1
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-06-29 00:37:30 -0700

    Added userLoginResponse dtos

---
+++ AuthService/src/main/java/com/runtimerebels/authservice/dtos/UserLoginResponse.java
@@ -0,0 +1,11 @@
+package com.runtimerebels.authservice.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserLoginResponse {
+    private String userId;
+    private String username;
+    private String email;
+    private String token;
+}
